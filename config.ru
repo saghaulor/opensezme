bff1cc324635f5039178f76a51a002847e69310d
Stephen Aghaulor <saghaulor@gmail.com>
2013-07-04 09:00:36 -0700

    These weren't ready to be in master.

--- config.ru
+++
@@ -1,3 +0,0 @@
-require File.expand_path("../config/boot.rb", __FILE__)
-require File.join(Dir.pwd, 'garage_door')
-run GarageDoor
--- garage_door.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-
-class GarageDoor < Sinatra::Base
-  set :root, File.dirname(__FILE__)
-  get '/' do
-    @status = 'derp'
-    "The garage door is currently: #{@status}"
-  end
-end
